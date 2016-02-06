9b10bb89ed5f150742be3de62e625d278388e727
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-02-06 10:17:25 -0600

    added response for #5-6 in the variables chapter

--- ch-variables.rb
+++ ch-variables.rb
@@ -1,3 +1,13 @@
-# 1
-# Write a program called name.rb that asks the user to type in their name and then prints out a greeting message with their name included.
+# 1-4 are in the files name.rb and age.rb
 
+# 5
+# What does x print to the screen in each case? Do they both give errors? Are the errors different? Why?
+
+puts "---\n#5\n---"
+puts "3, then an error because x was not defined in the second example."
+
+# 6
+# What does the following error message tell you?
+
+puts "---\n#6\n---"
+puts "The variable 'shoes' was not yet defined in the program."
