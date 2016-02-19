cea64b31904fd7471537165a227890b5955e2982
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-02-18 19:37:57 -0600

    added exercises from pseudocode chapter

---
+++ lesson_2/pseudo_code.rb
@@ -0,0 +1,47 @@
+# a method to return the sum of two integers
+# 1. request the first number from the user
+# 2. request the second number from the user
+# 3. convert numbers to integers
+# 4. add the two integers and return value to the user
+
+# START
+# GET number1
+# GET number2
+# SET number1, number2 to integers
+# SET result = number1 + number2
+# PRINT result
+# END
+
+###
+
+# a method that takes an array of strings, and returns a string that is all those strings concatenated together
+# 1. get the array
+# 2. iterate over the array, adding each value to a new string
+# 3. display the string
+
+# START
+# GET array
+# SET new_string = nil
+# WHILE iterator < length of array
+#   SET new_string = new_string + value from array at space "iterator"
+# PRINT new_string
+# END
+
+###
+
+# a method that takes an array of integers, and returns a new array with every other element
+# 1. get the array
+# 2. iterate over the even- or odd-numbered indexes of the array
+# 3. add each value from step 2 to a new array
+# 4. print the new array
+
+# START
+# GET array
+# SET new_array = nil
+# WHILE iterator < length of items in array
+#   IF index is odd-numbered
+#     SET new_array to new_array + string at the array's index
+#   ELSE
+#     do nothing
+# PRINT new_array
+# END
