eed1864d51568492fb04eb2f390f8caa8c348922
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-02-18 20:32:08 -0600

    added prompt method to calculator, refactored if/else statement to case/when

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,29 +1,29 @@
-# ask the user for two numbers
-# ask for an operation to perform
-# perform the operation on the two numbers
-# output result
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
 
-Kernel.puts("Welcome to calculator!")
+prompt("Welcome to calculator!")
 
 # get the first number
-Kernel.puts("Please enter the first number")
+prompt("What is the first number?")
 number1 = Kernel.gets().chomp()
 
 #get the second number 
-Kernel.puts("And the second number")
+prompt("And the second number")
 number2 = Kernel.gets().chomp()
 
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 operator = Kernel.gets().chomp()
 
-if operator == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operator == '2'
-  result = number1.to_i() - number2.to_i()
-elsif operator == '3'
-  result = number1.to_i() * number2.to_i()
-elsif operator == '4'
-  result = number1.to_f() / number2.to_f()
+result = case operator
+  when '1'
+    number1.to_i() + number2.to_i()
+  when '2'
+    number1.to_i() - number2.to_i()
+  when '3'
+    result = number1.to_i() * number2.to_i()
+  when '4'
+    result = number1.to_f() / number2.to_f()
 end
 
-Kernel.puts("Result: #{result}")
+prompt("Result: #{result}")
