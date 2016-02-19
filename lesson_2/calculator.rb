a5d7d4eb3857e81324846fd4d3ffd427eaef0c2b
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-02-18 22:56:50 -0600

    Removed internationalization

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,24 +1,3 @@
-require 'pry'
-
-# localizations
-localizations = {
-  en: {
-  welcome: "Welcome to calculator! What's your name?",
-  greeting: "Hi, ",
-  error_please_write_your_name: "Make sure to type in your name!",
-  first_number: "What is the first number?",
-  second_number: "What is the second number?",
-  error_enter_valid_number: "Please enter a valid number",
-  error_choose_correct_operator: "Must choose 1, 2, 3, or 4",
-  end_prompt: "Do you want to perform another calculation? Enter 'Y' for yes.",
-  end_thank_you: "Thank you for using me, bye!" }
-}
-
-def set_lang(option = 'en')
-  phrases = localizations[:en]
-  binding.pry
-end
-
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
@@ -48,20 +27,6 @@ def operation_to_message(op)
   end
   op
 end
-
-prompt("Do you speak/parlez-vous ENglish or/ou FRançais?")
-lang = ''
-loop do
-  lang = Kernel.gets().chomp()
-  set_lang(lang)
-
-  if lang.empty?()
-    prompt("Please type EN for English / Saisissez FR pour le français, s'il vous plaît")
-  else
-    break
-  end
-end
-
 
 prompt("Welcome to calculator! What's your name?")
 
