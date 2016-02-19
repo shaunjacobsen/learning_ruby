248b603248dd6e00ac95f488b51a7af3c8c1ea67
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-02-18 21:46:54 -0600

    Added functionality to ask user for her/his name

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -48,17 +48,15 @@ loop do
   end
 
   prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-  operator = Kernel.gets().chomp()
 
-  result = case operator
-    when '1'
-      number1.to_i() + number2.to_i()
-    when '2'
-      number1.to_i() - number2.to_i()
-    when '3'
-      result = number1.to_i() * number2.to_i()
-    when '4'
-      result = number1.to_f() / number2.to_f()
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3, or 4")
+    end
   end
 
   prompt("Result: #{result}")
