63886f1b15da0a60a2a89da0396b5f3f923d8609
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-02-18 21:42:15 -0600

    Added functionality to ask user for her/his name

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -6,44 +6,67 @@ def valid_number?(num)
   num.to_i() != 0
 end
 
-prompt("Welcome to calculator!")
+prompt("Welcome to calculator! What's your name?")
 
-number1 = '' # initialize the number outside the 'loop' block below
+name = ''
 loop do
-  prompt("What is the first number?")
-  number1 = Kernel.gets().chomp()
+  name = Kernel.gets().chomp()
 
-  if valid_number?(number1)
-    break
+  if name.empty?()
+    prompt("Make sure to type in your name!")
   else
-    prompt("Please enter a valid number")
+    break
   end
 end
 
-number2 = ''
+prompt("Hi, #{name}!")
+
+# main loop
 loop do
-  prompt("And the second number")
-  number2 = Kernel.gets().chomp()
+  number1 = '' # initialize the number outside the 'loop' block below
+  loop do
+    prompt("What is the first number?")
+    number1 = Kernel.gets().chomp()
 
-  if valid_number?(number2)
-    break
-  else
-    prompt("Please enter a valid number")
+    if valid_number?(number1)
+      break
+    else
+      prompt("Please enter a valid number")
+    end
   end
+
+  number2 = ''
+  loop do
+    prompt("And the second number")
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt("Please enter a valid number")
+    end
+  end
+
+  prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+  operator = Kernel.gets().chomp()
+
+  result = case operator
+    when '1'
+      number1.to_i() + number2.to_i()
+    when '2'
+      number1.to_i() - number2.to_i()
+    when '3'
+      result = number1.to_i() * number2.to_i()
+    when '4'
+      result = number1.to_f() / number2.to_f()
+  end
+
+  prompt("Result: #{result}")
+
+  prompt("Do you want to perform another calculation? Enter 'Y' for yes.")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+
 end
 
-prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-result = case operator
-  when '1'
-    number1.to_i() + number2.to_i()
-  when '2'
-    number1.to_i() - number2.to_i()
-  when '3'
-    result = number1.to_i() * number2.to_i()
-  when '4'
-    result = number1.to_f() / number2.to_f()
-end
-
-prompt("Result: #{result}")
+prompt("Thank you for using me, bye!")
