013b85145295910cb7f6c9377da6a75c75427b66
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-02-18 22:55:53 -0600

    Added some bonus features and an unsuccessful attempt at loading in the messages using a hash

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,9 +1,38 @@
+require 'pry'
+
+# localizations
+localizations = {
+  en: {
+  welcome: "Welcome to calculator! What's your name?",
+  greeting: "Hi, ",
+  error_please_write_your_name: "Make sure to type in your name!",
+  first_number: "What is the first number?",
+  second_number: "What is the second number?",
+  error_enter_valid_number: "Please enter a valid number",
+  error_choose_correct_operator: "Must choose 1, 2, 3, or 4",
+  end_prompt: "Do you want to perform another calculation? Enter 'Y' for yes.",
+  end_thank_you: "Thank you for using me, bye!" }
+}
+
+def set_lang(option = 'en')
+  phrases = localizations[:en]
+  binding.pry
+end
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def valid_number?(num)
-  num.to_i() != 0
+def number?(num)
+  integer?(num) || float?(num)
+end
+
+def integer?(num)
+  num.to_i.to_s == num
+end
+
+def float?(num)
+  num.to_f.to_s == num
 end
 
 def operation_to_message(op)
@@ -17,7 +46,22 @@ def operation_to_message(op)
   when '4'
     'Dividing'
   end
+  op
 end
+
+prompt("Do you speak/parlez-vous ENglish or/ou FRançais?")
+lang = ''
+loop do
+  lang = Kernel.gets().chomp()
+  set_lang(lang)
+
+  if lang.empty?()
+    prompt("Please type EN for English / Saisissez FR pour le français, s'il vous plaît")
+  else
+    break
+  end
+end
+
 
 prompt("Welcome to calculator! What's your name?")
 
@@ -41,7 +85,7 @@ loop do
     prompt("What is the first number?")
     number1 = Kernel.gets().chomp()
 
-    if valid_number?(number1)
+    if number?(number1)
       break
     else
       prompt("Please enter a valid number")
@@ -53,7 +97,7 @@ loop do
     prompt("And the second number")
     number2 = Kernel.gets().chomp()
 
-    if valid_number?(number2)
+    if number?(number2)
       break
     else
       prompt("Please enter a valid number")
@@ -83,15 +127,15 @@ loop do
 
   prompt("#{operation_to_message(operator)} the numbers...")
 
-  result = case operator
-    when '1'
-      number1.to_i() + number2.to_i()
-    when '2'
-      number1.to_i() - number2.to_i()
-    when '3'
-      result = number1.to_i() * number2.to_i()
-    when '4'
-      result = number1.to_f() / number2.to_f()
+  result =  case operator
+            when '1'
+              number1.to_i() + number2.to_i()
+            when '2'
+              number1.to_i() - number2.to_i()
+            when '3'
+              number1.to_i() * number2.to_i()
+            when '4'
+              number1.to_f() / number2.to_f()
   end
 
   prompt("Result: #{result}")
