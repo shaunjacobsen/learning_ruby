68f21eda771cce0adec373312b89127cd8c321b5
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-02-21 13:36:09 -0600

    Refactored game

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -4,6 +4,20 @@ def prompt(message)
   puts("=> #{message}")
 end
 
+def display_result(player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+     (player == 'paper' && computer == 'rock') ||
+     (player == 'scissors' && computer == 'paper')
+    prompt("You won!")
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'rock')
+    prompt("Computer won.")
+  else
+    prompt("It's a tie!")
+  end
+end
+
 loop do
   choice = ''
   loop do
@@ -21,17 +35,7 @@ loop do
 
   puts("You chose #{choice}.\nComputer chose #{computer_choice}.")
 
-  if (choice == 'rock' && computer_choice == 'scissors') ||
-     (choice == 'paper' && computer_choice == 'rock') ||
-     (choice == 'scissors' && computer_choice == 'paper')
-    prompt("You won!")
-  elsif (choice == 'rock' && computer_choice == 'paper') ||
-        (choice == 'paper' && computer_choice == 'scissors') ||
-        (choice == 'scissors' && computer_choice == 'rock')
-    prompt("Computer won.")
-  else
-    prompt("It's a tie!")
-  end
+  display_result(choice, computer_choice)
 
   prompt("Do you want to play again?")
   answer = gets.chomp
