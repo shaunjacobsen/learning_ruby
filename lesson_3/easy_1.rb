cd6fe4b36ee7416599ad0008e9d098f2295df59f
Shaun Jacobsen <shaunjacobsen@Shaun-Jacobsen.local>
2016-02-22 14:16:34 -0600

    Added easy answers 1

---
+++ lesson_3/easy_1.rb
@@ -0,0 +1,33 @@
+puts "1. [1, 2, 2, 3]"
+puts "\n\n"
+
+puts "2. ! at the end of a method usually signifies that the method will mutate the caller. A ? may signify that the method will return a true/false value.\n"
+puts "!= means 'does not equal'. ! before something negates it. ! after something mutates it. "
+puts "? is part of a shorthand way to create a one-line if/else statement. !! makes an object a boolean."
+puts "\n\n"
+
+puts "3. advice.gsub('important','urgent')"
+advice = "Few things in life are as important as house training your pet dinosaur."
+puts advice.gsub('important','urgent')
+puts "\n\n"
+
+puts "4. The first call deletes the number 2 from the array; the second deletes the number 1."
+puts "\n\n"
+
+puts "5. (10..100).cover?(42)"
+(10..100).cover?(42)
+puts "\n\n"
+
+puts "6. 'Four score and '.concat(famous_words)\n'Four score and ' + famous_words"
+puts "\n\n"
+
+puts "7. 42"
+puts "\n\n"
+
+puts "8. flintstones.flatten!"
+puts "\n\n"
+
+puts "9. flintstones.assoc('Barney')"
+puts "\n\n"
+
+puts "10. flintstones_hash = {}\nflintstones.each_with_index { |v, i| flintstones_hash[v] = i } "
