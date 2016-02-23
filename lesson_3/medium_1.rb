b638767098b7202fab2e6cbe39db614d7b516c28
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-02-22 19:49:39 -0600

    Finished medium 1 questions

--- lesson_3/medium_1.rb
+++ lesson_3/medium_1.rb
@@ -25,3 +25,67 @@ puts "the value of 40 + 2 is #{(40 + 2)}"
 # Second one: 1, 2
 
 # 5
+def factors(number)
+  dividend = number
+  divisors = []
+  loop do
+    if dividend > 0
+      divisors << number / dividend if number % dividend == 0
+      dividend -= 1
+    elsif dividend <= 0
+      break
+    end
+  end
+  divisors
+end
+
+p factors(0)
+p factors(144)
+
+# 6
+# The first method mutates the original array, whereas the second ones doesn't
+
+# 7
+def fib(first_num, second_num)
+  limit = 15
+  while second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(3, 2)
+p result
+
+# 8
+def titleize(phrase)
+  phrase.split.map { |w| w.capitalize }.join(" ")
+end
+
+puts titleize("The dog jumps over the log")
+
+# 9
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |k,v|
+  age_classification = case when munsters[k]["age"].between?(0, 18)
+    age_classification = "kid"
+  when munsters[k]["age"].between?(18, 64)
+    age_classification = "adult"
+  when munsters[k]["age"] > 64
+    age_classification = "senior"
+  else
+    age_classification = "unknown"
+  end
+  munsters[k]["age_group"] = age_classification
+end
+
+p munsters
