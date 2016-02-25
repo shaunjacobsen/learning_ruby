a5e8f79b06315bb893b6b1c98d8a5050220230ba
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-02-24 20:16:28 -0600

    Bonus feature 2

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -3,10 +3,13 @@ require 'pry'
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+POINTS_TO_END = 5 # the directions said not to use constants, but I feel like this is OK
 
 WINNING_LINES = [[1,2,3],[4,5,6],[7,8,9]] + # rows
                 [[2,5,8],[1,4,7],[3,6,9]] + # columns
                 [[1,5,9],[3,5,7]]           # diagonals
+
+score = { "player" => 0, "computer" => 0 }
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -77,6 +80,21 @@ def someone_won?(brd)
   !!detect_winner(brd) # !! turns the value into a boolean
 end
 
+def increment_score(recipient, value, score_hash)
+  score_hash[recipient] += value
+end
+
+def display_score(score_hash)
+  score = score_hash
+  player_score = score["player"]
+  computer_score = score["computer"]
+  prompt "Player #{player_score} || Computer #{computer_score}"
+end
+
+def end_game?(score_hash, points)
+  score_hash["player"] == points || score_hash["computer"] == points
+end
+
 def detect_winner(brd)
   WINNING_LINES.each do |line|
     if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
@@ -95,21 +113,31 @@ loop do
     display_board(board)
 
     player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    if someone_won?(board) || board_full?(board)
+      increment_score("player", 1, score)
+      break
+    end
 
     computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    if someone_won?(board) || board_full?(board)
+      increment_score("computer", 1, score)
+      break
+    end
   end
 
   if someone_won?(board)
     prompt "#{detect_winner(board)} won!"
+    display_score(score)
   else
     prompt "It's a tie."
   end
 
-  prompt "Play again? (y or n)"
+  break if end_game?(score, POINTS_TO_END)
+
+  prompt "Let's try to make it to #{POINTS_TO_END}. Wanna keep going? (y or n)"
   answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+
+  break if answer.start_with?('n')
 end
 
 prompt "Thanks for playing Tic-Tac-Toe!"
