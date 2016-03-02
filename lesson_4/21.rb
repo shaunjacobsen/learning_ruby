dc2658984bcd3e0e1a9774dbc47a8aaa24a2ec84
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-03-01 23:34:32 -0600

    fixed error where I incorrectly calculated the ace value

--- lesson_4/21.rb
+++ lesson_4/21.rb
@@ -15,10 +15,16 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-def get_cards(hand)
+def show_cards(hand)
   joined_hand = []
   hand.map { |c| joined_hand << c[1] }
   joined_hand.join(', ')
+end
+
+def obscure_cards(hand)
+  cards_to_hide = hand.drop(1)
+  unknown_string = cards_to_hide.length.times.collect { |x| ", unknown" }.join.to_s
+  hand[0][1] + unknown_string
 end
 
 def initialize_deck(cards, deck)
@@ -43,21 +49,33 @@ def deal(qty, hand, deck, recipient)
 end
 
 def determine_card_value(card, score)
-  if card[1] == "Ace"
-    if (score + 10) <= 21
-      card[2] = 11
+  card[2]
+end
+
+def hand_value(hand)
+  card_values = []
+  sorted_hand = hand.sort_by { |x| x[2] }.reverse
+  #binding.pry
+  sorted_hand.each do |c|
+    if c[1] == "Ace"
+      if (card_values.inject(:+) + 10) <= 21
+        c[2] = 11
+      else
+        c[2] = 1
+      end
+      card_values << c[2]
     else
-      card[2] = 1
+      card_values << c[2]
     end
   end
-  card[2]
+  card_values.inject(:+)
 end
 
 def dealer_hits?(score)
   score <= 17
 end
 
-def at_or_over_21?(score)
+def bust?(score)
   score >= 21
 end
 
@@ -85,31 +103,33 @@ initialize_deck(CARDS, deck)
 prompt "Dealing cards..."
 player_score = deal(2, player_cards, deck, player_score)
 dealer_score = deal(2, dealer_cards, deck, dealer_score)
-prompt "Your hand:"
-puts get_cards(player_cards)
+prompt "Your hand: #{show_cards(player_cards)}"
 
-prompt "Dealer's hand:"
-puts dealer_cards[0][1] + ", unknown"
+prompt "Dealer's hand: #{obscure_cards(dealer_cards)}"
 
-until (at_or_over_21?(player_score) || at_or_over_21?(dealer_score)) do
+until (bust?(player_score) || bust?(dealer_score)) do
   prompt "Hit [h] or Stay [s]?"
   answer = gets.chomp
 
   if answer.downcase.start_with?('s')
     if dealer_hits?(dealer_score)
+      prompt "Dealing card to dealer..."
       dealer_score = deal(1, dealer_cards, deck, dealer_score)
+      prompt "Dealer's hand: #{obscure_cards(dealer_cards)}"
     else break
     end
   elsif answer.downcase.start_with?('h')
+    prompt "Dealing card to you..."
     player_score = deal(1, player_cards, deck, player_score)
-    prompt "Your hand:"
-    puts get_cards(player_cards)
-    prompt "Your score:"
+    prompt "Your hand: #{show_cards(player_cards)}"
   end
+
+  #binding.pry
 end
 
-prompt "Your hand: #{get_cards(player_cards)}"
+prompt "TWENTY-ONE REACHED!\n"
+#prompt "Your hand: #{show_cards(player_cards)}"
 prompt "Your score: #{player_score}."
-prompt "Dealer's hand, revealed: #{get_cards(dealer_cards)}"
+prompt "Dealer's hand, revealed: #{show_cards(dealer_cards)}"
 prompt "Dealer's score: #{dealer_score}."
 puts who_won?(player_score, dealer_score)
