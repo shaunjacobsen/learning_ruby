a3801377734c6e75e30d2fc74244a3df297fa864
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-03-12 07:32:09 -0600

    gooddog exercise

--- oop_book/part1.rb
+++ oop_book/part1.rb
@@ -1,12 +1,29 @@
 class GoodDog
-  def initialize(name)
-    @name = name
+  attr_accessor :name, :height, :weight
+
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
   end
 
   def speak
     "#{@name} says woof!"
   end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{name} weighs #{weight} and is #{height} tall."
+  end
 end
 
-makoto = GoodDog.new("Makoto")
+makoto = GoodDog.new("Makoto", "15 inches", "25 lbs")
 puts makoto.speak
+puts makoto.info
+makoto.change_info("Mak", "16 inches", "30 lbs")
+puts makoto.info
