dd5ab99f2a5c7099d8f34c3fbc33d3e15f9e2b0e
Shaun Jacobsen <shaunjacobsen@Shaun-Jacobsen.local>
2016-03-31 15:27:55 -0500

    Final answers

--- oop_book/mycar.rb
+++ oop_book/mycar.rb
@@ -1,12 +1,25 @@
-class MyCar
+module Cargoable
+  def can_carry?(kg)
+    if kg <= 2000
+      puts "You can carry this as cargo."
+    else
+      puts "This cannot be carried as cargo."
+    end
+  end
+end
+
+class Vehicle
   attr_accessor :color, :speed
   attr_reader :year, :model
+
+  @@number_of_vehicles = 0
 
   def initialize(year, color, model)
     @year = year
     @color = color
     @model = model
     @speed = 0
+    @@number_of_vehicles += 1
   end
 
   def accelerate(amount)
@@ -34,6 +47,29 @@ class MyCar
     "This is a #{@year} #{@color} #{@model}."
   end
 
+  def self.number_of_vehicles
+    puts "There are #{@@number_of_vehicles} vehicle(s) on the road."
+  end
+
+  def age
+    puts "The #{@model} is #{calculate_age} years old."
+  end
+
+  private
+
+  def calculate_age
+    2016 - @year.to_i
+  end
+
+end
+
+class MyCar < Vehicle
+  WHOSE_CAR = "Shaun's"
+end
+
+class MyTruck < Vehicle
+  include Cargoable
+  WHOSE_TRUCK = "Not mine"
 end
 
 highlander = MyCar.new("2012", "Black", "Highlander")
@@ -41,3 +77,12 @@ highlander.spray_paint("Red")
 puts highlander.year
 MyCar.gas_mileage(400, 45)
 puts highlander
+Vehicle.number_of_vehicles
+truck = MyTruck.new("2015", "Grey", "F-150")
+Vehicle.number_of_vehicles
+highlander.age
+
+# Print to the screen the method lookup for the classes
+puts Vehicle.ancestors
+puts MyCar.ancestors
+puts MyTruck.ancestors
