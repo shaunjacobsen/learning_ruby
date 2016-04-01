83bdc79d5fd78c0bb808c39fb1801fdc89d838e7
Shaun Jacobsen <shaunjacobsen@Shaun-Jacobsen.local>
2016-04-01 11:46:48 -0500

    finished Collaborator Objects lesson.

--- oop/lesson_2.rb
+++ oop/lesson_2.rb
@@ -80,3 +80,25 @@ puts frenchie.speak
 puts frenchie.swim
 kitty = Cat.new
 puts kitty.speak
+
+
+### Lecture: Collaborator Objects
+
+class Person
+  attr_accessor :name, :pets
+
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+end
+
+bob = Person.new("Robert")
+
+kitty = Cat.new
+bud = Bulldog.new
+
+bob.pets << kitty
+bob.pets << bud
+
+p bob.pets
