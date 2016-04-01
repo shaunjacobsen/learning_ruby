cfb9b45248c45330d32ca440ac57cc15c9460985
Shaun Jacobsen <shaunjacobsen@Shaun-Jacobsen.local>
2016-04-01 10:24:00 -0500

    Starting the inheritance lecture exercises.

--- oop/lesson_2.rb
+++ oop/lesson_2.rb
@@ -1,32 +1,60 @@
-class Person
-  attr_accessor :first_name, :last_name
+### Lecture: Classes and Objects
 
-  def initialize(name)
-    parse_full_name(name)
+# class Person
+#   attr_accessor :first_name, :last_name
+
+#   def initialize(name)
+#     parse_full_name(name)
+#   end
+
+#   def name
+#     "#{@first_name} #{@last_name}"
+#   end
+
+#   def name=(name)
+#     parse_full_name(name)
+#   end
+
+#   def to_s
+#     name
+#   end
+
+#   private
+
+#   def parse_full_name(name)
+#     name_parts = name.split
+#     self.first_name = name_parts.first
+#     self.last_name = name_parts.size > 1 ? name_parts.last : ''
+#   end
+# end
+
+# bob = Person.new('Robert Smith')
+# rob = Person.new('Robert Smith')
+
+# puts "The person's name is #{bob}"
+
+
+### Lecture: Inheritance
+
+class Dog
+  def speak
+    'bark!'
   end
 
-  def name
-    "#{@first_name} #{@last_name}"
-  end
-
-  def name=(name)
-    parse_full_name(name)
-  end
-
-  def to_s
-    name
-  end
-
-  private
-
-  def parse_full_name(name)
-    name_parts = name.split
-    self.first_name = name_parts.first
-    self.last_name = name_parts.size > 1 ? name_parts.last : ''
+  def swim
+    'swimming!'
   end
 end
 
-bob = Person.new('Robert Smith')
-rob = Person.new('Robert Smith')
+class Bulldog < Dog
+  def swim
+    "can't swim!"
+  end
+end
 
-puts "The person's name is #{bob}"
+teddy = Dog.new
+puts teddy.speak
+puts teddy.swim
+frenchie = Bulldog.new
+puts frenchie.speak
+puts frenchie.swim
