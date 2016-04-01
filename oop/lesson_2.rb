05f6ade99febba9be6833a81e3c27ac1c105faf3
Shaun Jacobsen <shaunjacobsen@Shaun-Jacobsen.local>
2016-04-01 10:13:08 -0500

    commit 3

--- oop/lesson_2.rb
+++ oop/lesson_2.rb
@@ -10,6 +10,18 @@ class Person
   def name
     "#{@first_name} #{@last_name}"
   end
+
+  def name=(name)
+    name_parts = name.split
+    if name_parts.size > 1
+      @first_name = name_parts.first
+      @last_name = name_parts.last
+    elsif name_parts.size == 1
+      @first_name = name_parts.first
+    else
+      puts "Error"
+    end
+  end
 end
 
 bob = Person.new('Robert')
@@ -18,3 +30,7 @@ puts bob.first_name
 puts bob.last_name
 bob.last_name = 'Smith'
 puts bob.name
+
+bob.name = "John"
+puts bob.first_name
+puts bob.last_name
