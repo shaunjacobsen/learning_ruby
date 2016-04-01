3cc447c5580264e60d8323b0d5bddb3d6a87827c
Shaun Jacobsen <shaunjacobsen@Shaun-Jacobsen.local>
2016-04-01 10:09:10 -0500

    commit 2

--- oop/lesson_2.rb
+++ oop/lesson_2.rb
@@ -1,6 +1,20 @@
 class Person
-  attr_accessor :name
+  attr_accessor :first_name, :last_name
+
   def initialize(name)
-    @name = name
+    name_parts = name.split
+    @first_name = name_parts.first
+    @last_name = name_parts.size > 1 ? name_parts.last : ''
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
   end
 end
+
+bob = Person.new('Robert')
+puts bob.name
+puts bob.first_name
+puts bob.last_name
+bob.last_name = 'Smith'
+puts bob.name
