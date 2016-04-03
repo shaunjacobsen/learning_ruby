06d523ad148e34d66188bc37cfa59738b5b692df
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-03 16:44:54 -0500

    Easy 1 exercises completed

---
+++ oop/lesson_4_exercises.rb
@@ -0,0 +1,31 @@
+# Easy 1
+
+# Question 1
+# They are all objects. You can use '.class' on the objects to find out what class they belong to.
+
+# Question 2
+# Use 'include Speed' to mix the module into the class.
+
+# Question 3
+# self.class will print out the name of the class
+
+# Question 4
+# angry_cat = AngryCat.new
+
+# Question 5
+# Instance variables start with '@', so the Pizza class has an instance variable.
+
+# Question 6
+# add a 'get_volume' method that simply returns the instance variable
+
+# Question 7
+# It will print the object (e.g. "#<ThisClass:0x007faec20b4e10>")
+
+# Question 8
+# 'self' refers to the current object (the instance of the object)
+
+# Question 9
+# 'self' refers to the class (e.g. Cat)
+
+# Question 10
+# Bag.new('brown', 'canvas')
