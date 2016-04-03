0f3a4a32f797f198527e10ffee9186c86feb4c1d
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-03 09:52:35 -0500

    first commit

---
+++ oop/rps.rb
@@ -0,0 +1,90 @@
+class Player
+  attr_accessor :move
+
+  def initialize(player_type = :human)
+    @player_type = player_type
+    @move = nil
+  end
+
+  def choose
+    if human?
+      choice = nil
+      loop do
+        puts "Please choose rock, paper, or scissors:"
+        choice = gets.chomp
+        break if ['rock', 'paper', 'scissors'].include? choice
+        puts "Sorry, invalid choice"
+      end
+        self.move = choice
+    else
+      self.move = ['rock', 'paper', 'scissors'].sample
+    end
+  end
+
+  def human?
+    @player_type == :human
+  end
+end
+
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Player.new
+    @computer = Player.new(:computer)
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing!"
+  end
+
+  def display_winner
+    puts "You chose #{human.move}."
+    puts "The computer chose #{computer.move}."
+
+    case human.move
+    when 'rock'
+      puts "It's a tie!" if computer.move == 'rock'
+      puts "You won!" if computer.move == 'scissors'
+      puts "Computer won" if computer.move == 'paper'
+    when 'paper'
+      puts "It's a tie!" if computer.move == 'paper'
+      puts "You won!" if computer.move == 'rock'
+      puts "Computer won" if computer.move == 'scissors'
+    when 'scissors'
+      puts "It's a tie!" if computer.move == 'scissors'
+      puts "You won!" if computer.move == 'paper'
+      puts "Computer won" if computer.move == 'rock'
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y','n'].include? answer.downcase
+      puts "Sorry, must be y or n."
+    end
+
+    return true if answer == 'y'
+    return false
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
+    display_goodbye_message
+  end
+end
+
+RPSGame.new.play
