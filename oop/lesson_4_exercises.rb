37fdcfbb2ca2d6215d97980f50ac820c93276f3b
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-03 17:22:17 -0500

    Easy 2 exercises completed

--- oop/lesson_4_exercises.rb
+++ oop/lesson_4_exercises.rb
@@ -1,4 +1,4 @@
-# Easy 1
+### Easy 1
 
 # Question 1
 # They are all objects. You can use '.class' on the objects to find out what class they belong to.
@@ -29,3 +29,35 @@
 
 # Question 10
 # Bag.new('brown', 'canvas')
+
+### Easy 2
+
+# Question 1
+# The string "You will " will be returned with one of the 3 options from the array.
+
+# Question 2
+# The string "You will " will be returned with one of the 3 options from the choices method in the RoadTrip class, not the Oracle class.
+
+# Question 3
+# Use the '.ancestors' method on the class to see the lookup chain. For Orange it will look in itself, then Taste, Object, Kernel, and BasicObject. For Hotsouce it is the same.
+
+# Question 4
+# 'attr_accessor :type' will create implicit getter and setter methods for @type.
+
+# Question 5
+# excited_dog is a local variable; @excited_dog is an instance variable; @@excited_dog is a class variable.
+
+# Question 6
+# 'self.manufacturer' is a class method; it is prefixed with 'self'.
+
+# Question 7
+# @@cats_count keeps track of the number of objects created (initialized) from the Cat class.
+
+# Question 8
+# class Bingo < Game
+
+# Question 9
+# The 'play' method in the Bingo class would override the one in the Game class, but only for instances of the Bingo class, since the method lookup chain will look first in Bingo, then in Game.
+
+# Question 10
+# Keeps code cleaner and more compartamentalized, DRYs up the code, easier to manage complexity.
