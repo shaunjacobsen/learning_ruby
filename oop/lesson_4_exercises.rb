fb5c833995e0e4aeb4b00cb4156cca8ac5a58cb5
Shaun Jacobsen <shaunjacobsen@Shaun-Jacobsen.local>
2016-04-04 15:27:14 -0500

    Finished Hard 1 questions

--- oop/lesson_4_exercises.rb
+++ oop/lesson_4_exercises.rb
@@ -159,5 +159,116 @@ puts donut5
 # Question 7
 # Rename the 'light_information' method to 'information', since calling it requires writing 'Light.light_information', which is redundant.
 
-### 
+### Hard 1
+
+# Question 1
+class SecretFile
+  def initialize(secret_data, log)
+    @data = secret_data
+    @log = log
+  end
+
+  def data
+    @log.create_log_entry
+    @data
+  end
+end
+
+# Question 2
+
+module Range
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Range
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    @fuel_efficiency = km_traveled_per_liter
+    @fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures along with
+    super([20,20], 80, 8.0)
+  end
+end
+
+class Catamaran
+  include Range
+  attr_accessor :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    # ...
+  end
+end
+
+# Question 3
+
+class Boat
+  include Range
+  attr_accessor :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, fuel_capacity, fuel_efficiency)
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+    @fuel_efficiency = fuel_efficiency
+    @fuel_capacity = fuel_capacity
+  end
+end
+
+class Catamaran < Boat
+  include Range
+
+  def initialize(num_propellers, num_hulls, km_per_liter, liters_of_fuel_capacity)
+    super
+  end
+end
+
+class Motorboat < Boat
+  def initialize(km_per_liter, liters_of_fuel_capacity)
+    super(1, 1, km_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+# Question 4
+class Boat
+  include Range
+  attr_accessor :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, fuel_capacity, fuel_efficiency)
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+    @fuel_efficiency = fuel_efficiency
+    @fuel_capacity = fuel_capacity
+  end
+
+  def range
+    (@fuel_capacity * @fuel_efficiency) + 10
+  end
+
+end
 
