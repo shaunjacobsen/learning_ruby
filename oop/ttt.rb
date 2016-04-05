57a31bd72c4596cc57591a5d9e52c9f99bf5c724
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-04 21:04:56 -0500

    removed Board#set_square_at and replaced with Board#[]

--- oop/ttt.rb
+++ oop/ttt.rb
@@ -24,8 +24,8 @@ class Board
     puts "     |     |"
   end
 
-  def set_square_at(key, marker)
-    @squares[key].marker = marker
+  def []=(num, marker)
+    @squares[num].marker = marker
   end
 
   def unmarked_keys
@@ -136,11 +136,11 @@ class TTTGame
       puts "Sorry, not a valid choice. Try again:"
     end
 
-    board.set_square_at(square, human.marker)
+    board[square] = human.marker
   end
 
   def computer_moves
-    board.set_square_at(board.unmarked_keys.sample, computer.marker)
+    board[board.unmarked_keys.sample] = computer.marker
   end
 
   def display_result
