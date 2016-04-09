c1854c8dd4291788d76de363f17e178b6797855f
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-09 11:51:38 -0500

    added deal_cards method

--- oop/21.rb
+++ oop/21.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-class Cards
+class Card
   SUITS = %w(Hearts Clubs Spades Diamonds)
   FACES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
@@ -16,8 +16,8 @@ class Deck
 
   def initialize
     @cards = []
-    Card::SUITS.each do |card|
-      CARD::FACES.each do |face|
+    Card::SUITS.each do |suit|
+      Card::FACES.each do |face|
         @cards << Card.new(suit, face)
       end
     end
@@ -28,19 +28,26 @@ class Deck
     cards.shuffle!
   end
 
-  def deal(quantity)
-    quantity.times do |_|
-      cards.pop
-    end
+  def deal
+    cards.pop
   end
 
 end
 
 class Player
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+  end
 
 end
 
-class Dealer
+class Human < Player
+
+end
+
+class Dealer < Player
 
 end
 
@@ -49,5 +56,46 @@ class Hand
 end
 
 class TwentyOne
+  attr_accessor :deck, :human, :dealer
+
+  def initialize
+    @deck = Deck.new
+    @human = Human.new
+    @dealer = Dealer.new
+  end
+
+  def start
+    deal_cards
+    show_initial_cards
+    player_turn
+    dealer_turn
+    show_result
+  end
+
+  def deal_cards
+    [human, dealer].each do |participant|
+      2.times do |_|
+        participant.cards << deck.deal
+      end
+    end
+    p human.cards
+    p dealer.cards
+  end
+
+  def show_initial_cards
+  end
+
+  def player_turn
+  end
+
+  def dealer_turn
+  end
+
+  def show_result
+  end
+
 
 end
+
+game = TwentyOne.new
+game.start
