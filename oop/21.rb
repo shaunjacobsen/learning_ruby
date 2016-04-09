79a466aba9f0ed47f10071201d723805ba3bf957
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-09 11:34:03 -0500

    starting fresh

--- oop/21.rb
+++ oop/21.rb
@@ -1,70 +1,53 @@
 require 'pry'
 
-class Hand
-  attr_accessor :hand_array
-  def initialize
-    2.times do
-      @hand_array << @game_deck.deal
-    end
-  end
+class Cards
+  SUITS = %w(Hearts Clubs Spades Diamonds)
+  FACES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
-  def show
+  def initialize(suit, face)
+    @suit = suit
+    @face = face
   end
 
 end
 
 class Deck
-  attr_accessor :deck
-
-  CARDS = [["2", 2], ["3", 3], ["4", 4], ["5", 5], ["6", 6], ["7", 7], ["8", 8], ["9", 9], ["10", 10]] +
-          [["Jack", 10], ["Queen", 10], ["King", 10], ["Ace", 0]]
+  attr_accessor :cards
 
   def initialize
-    @deck = []
-    suits = %w(Hearts Spades Clubs Diamonds)
-    suits.each do |suit|
-      CARDS.each do |card|
-        deck << [suit, card].flatten
+    @cards = []
+    Card::SUITS.each do |card|
+      CARD::FACES.each do |face|
+        @cards << Card.new(suit, face)
       end
     end
+    rearrange
   end
 
-  def deal
-    deck.shuffle!.pop
+  def rearrange
+    cards.shuffle!
   end
 
-end
-
-class Card
+  def deal(quantity)
+    quantity.times do |_|
+      cards.pop
+    end
+  end
 
 end
 
 class Player
-  attr_accessor :player_type, :player_hand
 
-  def initialize(player_type)
-    @player_type = player_type
-    @player_hand = Hand.new
-  end
 end
 
-class Game
-  attr_accessor :game_deck
+class Dealer
 
-  def initialize
-    @game_deck = Deck.new
-    @human = Player.new('human')
-    @dealer = Player.new('dealer')
-  end
-
-  def start
-    
-    
-    #show_initial_cards
-    #player_turn
-    #dealer_turn
-    #show_result
-  end
 end
 
-Game.new.start
+class Hand
+
+end
+
+class TwentyOne
+
+end
