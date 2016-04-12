0c8954233404425f4f380e7bd724b02636f3f952
Shaun Jacobsen <shaunjacobsen@Shaun-Jacobsen.local>
2016-04-12 09:54:36 -0500

    adding file

---
+++ minitest/todos.rb
@@ -0,0 +1,222 @@
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+class TodoList
+  attr_accessor :title, :todos
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def add(item)
+    todos << item
+  end
+
+  def size
+    todos.size
+  end
+
+  def first
+    todos.first
+  end
+
+  def last
+    todos.last
+  end
+
+  def item_at(index)
+    todos[index]
+  end
+
+  def mark_done_at(index)
+    todos[index].done!
+  end
+
+  def mark_undone_at(index)
+    todos[index].undone!
+  end
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(index)
+    todos.delete_at(index)
+  end
+
+  def to_s
+    todos.each { |todo| puts "#{todo}" }
+  end
+
+  def each
+    todos.each do |todo|
+      yield(todo)
+    end
+    self
+  end
+
+  def select
+    list = TodoList.new(title)
+    each do |todo|
+      list.add(todo) if yield(todo)
+    end
+    list
+  end
+
+  def find_by_title(string)
+    todos.select { |todo| todo.title == string }.first
+  end
+
+  def all_done
+    todos.select { |todo| todo.done? }
+  end
+
+  def all_not_done
+    todos.select { |todo| !todo.done? }
+  end
+
+  def mark_done(string)
+    find_by_title(string) && find_by_title(string).done!
+  end
+
+  def mark_all_done
+    each do |todo|
+      todo.done!
+    end
+  end
+
+  def mark_all_undone
+    each do |todo|
+      todo.undone!
+    end
+  end
+end
+
+# given
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+todo4 = Todo.new("Learn to program")
+list = TodoList.new("Today's Todos")
+
+# ---- Adding to the list -----
+
+# add
+list.add(todo1)                 # adds todo1 to end of list, returns list
+list.add(todo2)                 # adds todo2 to end of list, returns list
+list.add(todo3)                 # adds todo3 to end of list, returns list
+list.add(todo4)
+list.add(1)                     # raises TypeError with message "Can only add Todo objects"
+
+# <<
+# same behavior as add
+
+# ---- Interrogating the list -----
+
+# size
+list.size                       # returns 3
+
+# first
+list.first                      # returns todo1, which is the first item in the list
+
+# last
+list.last                       # returns todo3, which is the last item in the list
+
+# ---- Retrieving an item in the list ----
+
+# item_at
+#list.item_at                    # raises ArgumentError
+list.item_at(1)                 # returns 2nd item in list (zero based index)
+list.item_at(100)               # raises IndexError
+
+# ---- Marking items in the list -----
+
+# mark_done_at
+#list.mark_done_at               # raises ArgumentError
+list.mark_done_at(1)            # marks the 2nd item as done
+#list.mark_done_at(100)          # raises IndexError
+
+# mark_undone_at
+#list.mark_undone_at             # raises ArgumentError
+list.mark_undone_at(1)          # marks the 2nd item as not done,
+#list.mark_undone_at(100)        # raises IndexError
+
+# ---- Deleting from the the list -----
+
+# shift
+list.shift                      # removes and returns the first item in list
+
+# pop
+list.pop                        # removes and returns the last item in list
+
+# remove_at
+#list.remove_at                  # raises ArgumentError
+#list.remove_at(1)               # removes and returns the 2nd item
+list.remove_at(100)             # raises IndexError
+
+# ---- Outputting the list -----
+
+# to_s
+list.to_s                      # returns string representation of the list
+
+list.mark_done_at(1)
+list.mark_done_at(0)
+
+puts "---"
+
+list.each do |todo|
+  puts todo
+end
+
+results = list.select { |todo| todo.done? }
+
+list.find_by_title("Learn to program")
+list.all_done
+list.all_not_done
+list.mark_all_done
+list.to_s
+list.mark_all_undone
+list.to_s
+
+# ---- Today's Todos ----
+# [ ] Buy milk
+# [ ] Clean room
+# [ ] Go to gym
+
+# or, if any todos are done
+
+# ---- Today's Todos ----
+# [ ] Buy milk
+# [X] Clean room
+# [ ] Go to gym
