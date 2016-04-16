77951ae22c7e3640cd734fdf67a07c1e5941d24b
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-16 08:42:56 -0500

    removed skips

--- challenges/test_grade_school.rb
+++ challenges/test_grade_school.rb
@@ -1,5 +1,7 @@
 require 'minitest/autorun'
 require_relative 'grade_school'
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 class SchoolTest < Minitest::Test
   attr_reader :school
@@ -13,13 +15,11 @@ class SchoolTest < Minitest::Test
   end
 
   def test_add_student
-    skip
     school.add('Aimee', 2)
     assert_equal({ 2 => ['Aimee'] }, school.to_h)
   end
 
   def test_add_more_students_in_same_class
-    skip
     school.add('Blair', 2)
     school.add('James', 2)
     school.add('Paul', 2)
@@ -27,14 +27,12 @@ class SchoolTest < Minitest::Test
   end
 
   def test_add_students_to_different_grades
-    skip
     school.add('Chelsea', 3)
     school.add('Logan', 7)
     assert_equal({ 3 => ['Chelsea'], 7 => ['Logan'] }, school.to_h)
   end
 
   def test_get_students_in_a_grade
-    skip
     school.add('Bradley', 5)
     school.add('Franklin', 5)
     school.add('Jeff', 1)
@@ -42,12 +40,10 @@ class SchoolTest < Minitest::Test
   end
 
   def test_get_students_in_a_non_existant_grade
-    skip
     assert_equal [], school.grade(1)
   end
 
   def test_sort_school # rubocop:disable Metrics/MethodLength
-    skip
     [
       ['Jennifer', 4], ['Kareem', 6],
       ['Christopher', 4], ['Kyle', 3]
