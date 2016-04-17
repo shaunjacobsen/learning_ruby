862051cef67d6db6ac6e9d62e449745d69e3bc7a
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-17 09:21:08 -0500

    some refactoring

--- challenges/pig_latin.rb
+++ challenges/pig_latin.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class PigLatin
   VOWELS = %w(a e i o u)
   CONSONANTS = %w(b c d f g h j k l m n p q r s t v w x y z)
@@ -7,6 +5,10 @@ class PigLatin
   THREE_LETTER_EXCEPTIONS = %w(thr sch)
 
   def initialize
+  end
+
+  def self.add(word, string='ay')
+    new_word = word + string
   end
 
   def self.translate(word_string)
@@ -21,8 +23,7 @@ class PigLatin
         translated_phrase << new_word
       when TWO_LETTER_EXCEPTIONS.include?(word.split('').slice(0..1).join)
         word += word.slice!(0..1)
-        new_word = word + 'ay'
-        translated_phrase << new_word
+        translated_phrase << add(word)
       when word.include?('qu')
         letters_around_qu = word.split('qu')
         new_word = letters_around_qu[1] + letters_around_qu[0] + 'quay'
@@ -30,12 +31,10 @@ class PigLatin
       when word.start_with?('ye')
         word.slice!(0)
         translated_phrase << word + 'yay'
-      when word.start_with?('yt')
-        translated_phrase << word + 'ay'
-      when word.start_with?('xr')
-        translated_phrase << word + 'ay'
+      when word.start_with?('yt', 'xr')
+        translated_phrase << add(word)
       when VOWELS.include?(first_letter)
-        translated_phrase << word + 'ay'
+        translated_phrase << add(word)
       when CONSONANTS.include?(first_letter)
         letters = word.split('')
         letters << letters.shift
@@ -47,5 +46,3 @@ class PigLatin
   end
 
 end
-
-p PigLatin.translate('yellow yttria xenon xray')
