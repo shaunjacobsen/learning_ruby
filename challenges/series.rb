c5c015c2b4086233c0ee06c8e942e25d69adde60
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-17 08:08:18 -0500

    all tests passed

--- challenges/series.rb
+++ challenges/series.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Series
   attr_accessor :series
 
@@ -5,15 +7,25 @@ class Series
     num_to_array = num.split('')
     array_of_numbers = []
     num_to_array.each do |num|
-      array_of_numbers << [num.to_i]
+      array_of_numbers << num.to_i
     end
     @series = array_of_numbers
   end
 
   def slices(num)
-    series
+    unless num <= series.length
+      raise ArgumentError.new("The requested slice length must be at or below the length of the number passed.")
+    end
+    sliced_nums = []
+    series.size.times do
+      sliced_nums << series.slice(0, num)
+      series.shift
+    end
+    sliced_nums.delete_if { |slice| slice.length < num }
+    sliced_nums
   end
 end
 
-series = Series.new('1234')
+series = Series.new('982347')
 p series.series
+p series.slices(3)
