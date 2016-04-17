d8fcecf9708f5fe390f16c1729e254e5d843899b
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-17 09:11:01 -0500

    first draft

--- challenges/pig_latin.rb
+++ challenges/pig_latin.rb
@@ -1,12 +1,51 @@
+require 'pry'
+
 class PigLatin
-  attr_accessor :words
+  VOWELS = %w(a e i o u)
+  CONSONANTS = %w(b c d f g h j k l m n p q r s t v w x y z)
+  TWO_LETTER_EXCEPTIONS = %w(ch qu th)
+  THREE_LETTER_EXCEPTIONS = %w(thr sch)
 
   def initialize
   end
 
-  def translate(words)
-    @words = words.split(' ')
-    
+  def self.translate(word_string)
+    @words = word_string.split(' ')
+    translated_phrase = []
+    @words.each do |word|
+      first_letter = word.split('').first
+      case
+      when THREE_LETTER_EXCEPTIONS.include?(word.split('').slice(0..2).join)
+        word += word.slice!(0..2)
+        new_word = word + 'ay'
+        translated_phrase << new_word
+      when TWO_LETTER_EXCEPTIONS.include?(word.split('').slice(0..1).join)
+        word += word.slice!(0..1)
+        new_word = word + 'ay'
+        translated_phrase << new_word
+      when word.include?('qu')
+        letters_around_qu = word.split('qu')
+        new_word = letters_around_qu[1] + letters_around_qu[0] + 'quay'
+        translated_phrase << new_word
+      when word.start_with?('ye')
+        word.slice!(0)
+        translated_phrase << word + 'yay'
+      when word.start_with?('yt')
+        translated_phrase << word + 'ay'
+      when word.start_with?('xr')
+        translated_phrase << word + 'ay'
+      when VOWELS.include?(first_letter)
+        translated_phrase << word + 'ay'
+      when CONSONANTS.include?(first_letter)
+        letters = word.split('')
+        letters << letters.shift
+        letters << ['a', 'y']
+        translated_phrase << letters.join
+      end
+    end
+    translated_phrase.join(' ')
   end
 
 end
+
+p PigLatin.translate('yellow yttria xenon xray')
