08ef45d86db42e688dfae62eea87c980b725f189
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-04-17 07:41:50 -0500

    delivering an array of sliced numbers

--- challenges/series.rb
+++ challenges/series.rb
@@ -1,8 +1,19 @@
 class Series
+  attr_accessor :series
 
   def initialize(num)
+    num_to_array = num.split('')
+    array_of_numbers = []
+    num_to_array.each do |num|
+      array_of_numbers << [num.to_i]
+    end
+    @series = array_of_numbers
   end
 
   def slices(num)
+    series
   end
 end
+
+series = Series.new('1234')
+p series.series
