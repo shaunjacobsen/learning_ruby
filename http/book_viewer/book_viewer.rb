5521181ebcbc83290d4ceee147e82bed645e4f60
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-05-15 11:59:17 -0500

    added search functionality

--- http/book_viewer/book_viewer.rb
+++ http/book_viewer/book_viewer.rb
@@ -4,14 +4,22 @@ require "sinatra/reloader"
 
 helpers do
   def in_paragraphs(text)
-    text.split("\n\n").map do |line|
-      "<p>#{line}</p>"
+    text.split("\n\n").each_with_index.map do |line, index|
+      "<p id='p#{index + 1}'>#{line}</p>"
     end.join
+  end
+
+  def highlight_search_terms(text, paragraph)
+    paragraph.gsub(text, "<span class='highlight'>#{text}</span>")
   end
 end
 
 before do
   @toc = File.readlines("data/toc.txt")
+end
+
+not_found do
+  redirect "/"
 end
 
 get "/" do
@@ -28,6 +36,18 @@ get "/chapters/:number" do
   erb :chapter
 end
 
-not_found do
-  redirect "/"
+get "/search" do
+  if params[:query]
+    @results = @toc.each_with_index.each_with_object([]) do |(chapter, index), results|
+      text = File.read("data/chp#{index + 1}.txt")
+      paragraphs = text.split("\n\n")
+      paragraphs.each_with_index do |paragraph, paragraph_index|
+        if paragraph.include?(params[:query])
+          results << [chapter, index, paragraph, paragraph_index]
+        end
+      end
+    end
+  end
+
+  erb :search
 end
