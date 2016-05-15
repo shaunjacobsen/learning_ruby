f1b3489a4d749339d179ccc6d049cfe14c252e29
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-05-15 08:13:48 -0500

    last commit for this file

--- http/echo.rb
+++ http/echo.rb
@@ -4,7 +4,7 @@ def parse_request(request_line)
   http_method, path_and_params, http = request_line.split(" ")
   path, params = path_and_params.split("?")
 
-  params = params.split("&").each_with_object({}) do |pair, hash|
+  params = (params || "").split("&").each_with_object({}) do |pair, hash|
     key, value = pair.split("=")
     hash[key] = value
   end
@@ -36,14 +36,14 @@ loop do
   client.puts params
   client.puts "</pre>"
 
-  client.puts "<h1>Rolls!</h1>"
-  rolls = params["rolls"].to_i
-  sides = params["sides"].to_i
+  client.puts "<h1>Counter</h1>"
 
-  rolls.times do
-    roll = rand(sides) + 1
-    client.puts "<p>", roll, "</p>"
-  end
+  number = params["number"].to_i
+
+  client.puts "<p>The current number is #{number}.</p>"
+
+  client.puts "<a href='?number=#{number + 1}'>Add one</a>"
+  client.puts "<a href='?number=#{number - 1}'>Subtract one</a>"
 
   client.puts "</body>"
   client.puts "</html>"
