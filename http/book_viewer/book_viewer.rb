6e9b8b9b008982fe26bcc92ab63e802571f8d638
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-05-15 08:35:35 -0500

    added list of table of contents

--- http/book_viewer/book_viewer.rb
+++ http/book_viewer/book_viewer.rb
@@ -3,6 +3,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  @title = "This is the title"
+  @title = "The Adventures of Sherlock Holmes"
+  @toc = File.readlines("data/toc.txt")
   erb :home
 end
