92ee5daf0271901d5f41eff4412bf7644c4e626a
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-05-15 11:19:34 -0500

    moved @toc to before block

--- http/book_viewer/book_viewer.rb
+++ http/book_viewer/book_viewer.rb
@@ -2,15 +2,17 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @toc = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @toc = File.readlines("data/toc.txt")
   erb :home
 end
 
 get "/chapters/:number" do
   @chapter_num = params[:number].to_i
-  @toc = File.readlines("data/toc.txt")
   @chapter_name = @toc[@chapter_num - 1]
   @title = "Chapter #{@chapter_num}: #{@chapter_name}"
   @chapter = File.read("data/chp#{@chapter_num}.txt")
