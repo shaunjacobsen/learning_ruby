6ee5782978008be0c04ccdbb8a5d7be6c589d9d9
Shaun Jacobsen <shaunjacobsen@Shaun-Jacobsen.local>
2016-05-17 11:12:41 -0500

    fulfilled all requirements

--- http/users/users.rb
+++ http/users/users.rb
@@ -3,11 +3,38 @@ require "sinatra"
 require "sinatra/reloader"
 require "yaml"
 
+helpers do
+  def count_interests(hash)
+    interests = []
+    hash.each do |_, v|
+      interests << v[:interests]
+    end
+    interests.flatten!
+  end
+end
+
 before do
-  @users = YAML.load_file('users.yaml').to_h
+  @users = YAML.load_file('users.yaml')
+  @num_interests = count_interests(@users).size
+end
+
+not_found do
+  redirect "/"
 end
 
 get "/" do
   @title = "Users"
+
   erb :home
+end
+
+get "/user/:name" do
+  @title = params[:name]
+  @active_user = @users.select { |k, v| k.to_s == params[:name] }.first
+  @info = @active_user
+  @username = @info.first
+  @email = @info[1][:email]
+  @interests = @info[1][:interests].join(", ")
+
+  erb :user
 end
