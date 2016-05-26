9af4c75e8619b53195304b110fcf9638ea6ed4f6
Shaun Jacobsen <shaunjacobsen@Shaun-Jacobsen.local>
2016-05-25 19:39:43 -0500

    saving work in progress

--- http/todos/todo.rb
+++ http/todos/todo.rb
@@ -2,14 +2,24 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+# configuration
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+# routes
+
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    {name: "Lunch Groceries", todos: []},
-    {name: "Dinner Groceries", todos: []}
-  ]
+  @lists = session[:lists]
   erb :lists, layout: :layout
 end
