4b6e77d92d6edee82d39d79951f3e018e061e283
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-05-25 20:56:02 -0500

    added validation

--- http/todos/todo.rb
+++ http/todos/todo.rb
@@ -16,10 +16,29 @@ end
 # routes
 
 get "/" do
-  redirect "/lists"
+  redirect "/lists"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 end
 
+# view all lists
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
+end
+
+# creates a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+  if (1..100).cover? list_name.size
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = "The list \"#{list_name}\" has been created."
+    redirect "/lists"
+  else
+    session[:error] = "The list name must be between 1 and 100 characters"
+    erb :new_list, layout: :layout
+  end
+end
+
+# renders new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
 end
