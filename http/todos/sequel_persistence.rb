240c9c851eb46b3cb4107e12c281efcb5e040e1d
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2016-07-04 13:32:10 -0500

    finished converting all methods in SequelPersistence

--- http/todos/sequel_persistence.rb
+++ http/todos/sequel_persistence.rb
@@ -6,12 +6,7 @@ class SequelPersistence
     @db = Sequel.connect('postgres://localhost/todos')
     @db.loggers << logger
   end
-
-  def query(statement, *params)
-    @logger.info "#{statement}: #{params}"
-    @db.exec_params(statement, params)
-  end
-
+  
   def find_todos_for_list(list_id)
     @db[:todos].where(list_id: list_id)
   end
@@ -32,45 +27,32 @@ class SequelPersistence
   end
 
   def create_new_list(list_name)
-    query("INSERT INTO lists (name) VALUES ($1)", list_name)
+    @db[:lists].insert(name: list_name)
   end
 
   def delete_list(id)
-    query("DELETE FROM todos WHERE list_id = $1", id)
-    query("DELETE FROM lists WHERE id = $1", id)
+    @db[:todos].where(list_id: id).delete
+    @db[:lists].where(id: id).delete
   end
 
   def update_list_name(id, new_name)
-    query("UPDATE lists SET name = $1 WHERE id = $2", new_name, id)
+    @db[:lists].where(id: id).update(name: new_name)
   end
 
   def create_new_todo(list_id, todo_name)
-    query("INSERT INTO todos (list_id, name) VALUES ($1, $2)",
-          list_id, todo_name)
+    @db[:todos].insert(list_id: list_id, name: todo_name)
   end
 
   def delete_todo_from_list(list_id, todo_id)
-    query("DELETE FROM todos WHERE id = $1 AND list_id = $2", todo_id, list_id)
+    @db[:todos].where(id: todo_id, list_id: list_id).delete
   end
 
   def update_todo_status(list_id, todo_id, status)
-    query("UPDATE todos SET completed = $1 WHERE id = $2 AND list_id = $3",
-          status, todo_id, list_id)
+    @db[:todos].where(id: todo_id, list_id: list_id).update(completed: status)
   end
 
   def mark_all_todos_as_completed(list_id)
-    query("UPDATE todos SET completed = true WHERE list_id = $1", list_id)
-  end
-
-  private
-
-  def tuple_to_list_hash(tuple)
-    {
-      id: tuple['id'].to_i,
-      name: tuple['name'],
-      todos_count: tuple['todos_count'].to_i,
-      todos_remaining: tuple['todos_remaining'].to_i
-    }
+    @db[:todos].where(list_id: list_id).update(completed: true)
   end
 
 end
